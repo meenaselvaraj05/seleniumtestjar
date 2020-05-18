89edf79f4dcd4f5c26198a0ac3f31b8b4f3dd9e5
meena <meena.selvaraj05@gmail.com>
2020-05-18 14:32:05 +0000

    Added a failure

---
+++ TestWithSelinumFailure.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+public class TestWithSelinumFailure{
+ 
+        public static void main(String[] args) throws IOException, InterruptedException {
+                System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/chromeDriver/chromedriver");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+ 
+                WebDriver driver = new ChromeDriver(chromeOptions);
+ 
+                driver.get("http://34.69.63.240:7000/");
+				driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+				driver.findElement(By.linkText("About Us")).click();
+		
+				String actualString = driver.findElement(By.xpath("//*[@id=\"PID-ab2-pg\"]/b")).getText();
+ 
+                Thread.sleep(1000);
+				String expectedString = "aboutss";
+ 
+                if (actualString.contains(expectedString)) {
+                        System.out.println("Pass");
+                } else {
+                        System.out.println("Fail");
+                }
+                driver.quit();
+        }
+}
